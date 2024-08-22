a4264d6d67afe698a92dede22a281e176287e835
Deyan Svetoslavov <dsvetoslavov@gmail.com>
2024-08-22 07:06:17 +0100

    Add middleware to parse form and WIP on storing a meal in a file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const mealInformation = require("./meal-information.js");
+const fs = require("fs");
 
 const port = 3000;
 
@@ -9,6 +10,8 @@ const app = express();
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
+
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
   const foodInformations = mealInformation.foodInformations;
@@ -26,8 +29,21 @@ app.get("/new-meal", (req, res) => {
   });
 });
 
-app.post("/new-meal", (req, res) => {
-  console.log(req.formData);
+app.post("/new-meal", (req, res, next) => {
+  const foodInformations = mealInformation.foodInformations;
+
+  console.log(req.body.food);
+  console.log(req.body["food-quantity"]);
+
+  if (!fs.existsSync("./meals.json")) {
+    fs.writeFileSync('./meals.json', '[]');
+  }
+
+  const mealsRaw = fs.readFileSync("./meals.json");
+  const meals = JSON.parse(mealsRaw);
+  console.log(meals);
+
+  res.status(200).render("new-meal", { foodInformations });
 });
 
 app.listen(port, () => {
